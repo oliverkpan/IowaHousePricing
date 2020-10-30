0777a9e67321cbf8d8d188514ca8d0643e1832c2
Oliver Pan <oliver.pan@mail.utoronto.ca>
2020-10-29 22:04:27 -0400

    Add files via upload

--- deploy.py
+++ deploy.py
@@ -4,25 +4,25 @@ import pickle
 import streamlit as st 
 from PIL import Image 
 
-pickle_in = open('/Users/oliverpan/Desktop/catboost.pkl', 'rb') 
+pickle_in = open('/Users/oliverpan/Desktop/catboost_updated3.pkl', 'rb') 
 classifier = pickle.load(pickle_in) 
   
 def welcome(): 
     return 'welcome all'
 
-def prediction(overall_qual, area, year_built):   
+def prediction(overall_qual, lot_area, garage_yr, garage_cars, bsmt_qual, sale_condition):   
    
     prediction = classifier.predict( 
-        [[overall_qual, area, year_built]]) 
+        [[overall_qual, lot_area, garage_yr, garage_cars, bsmt_qual, sale_condition]]) 
     print(prediction) 
     return prediction 
 
 def main(): 
-    st.title("Prediction House Prices in Iowa") 
+    st.title("Predicting House Prices in Iowa") 
       
     html_temp = """ 
-    <div style ="background-color:red;padding:13px"> 
-    <h1 style ="color:black;text-align:center;">Streamlit App</h1> 
+    <div style ="background-color:green;padding:13px"> 
+    <h1 style ="color:black;text-align:center;">Iowatch</h1> 
     </div> 
     """
       
@@ -32,17 +32,20 @@ def main():
       
     # the following lines create text boxes in which the user can enter  
     # the data required to make the prediction 
-    overall_qual = st.text_input("Overall Qual", "") 
-    area = st.text_input("Area", "") 
-    year_built = st.text_input("Year Built", "") 
-    result ="" 
+    overall_qual = st.slider('Overall Qual', min_value = 0, max_value = 10, key = '1')
+    bsmt_qual = st.slider('Basement Quality', min_value = 0, max_value = 10, key = '2')
+    sale_condition = st.slider('Sale Quality', min_value = 0, max_value = 10, key = '3')
+    lot_area = st.text_input("Lot Area", "") 
+    garage_yr = st.text_input("Year Built", "")
+    garage_cars = st.selectbox('Number of Garages', options = ['1','2','3','4'])
+    result = "" 
       
     # the below line ensures that when the button called 'Predict' is clicked,  
     # the prediction function defined above is called to make the prediction  
     # and store it in the variable result 
     if st.button("Predict"): 
-        result = prediction(overall_qual, area, year_built) 
-    st.success('The output is {}'.format(result)) 
+        result = prediction(overall_qual, lot_area, garage_yr, garage_cars, bsmt_qual, sale_condition) 
+    st.success('The predicted house price is {}'.format(result)) 
      
 if __name__=='__main__': 
     main() 
